08d82f041551f0ece9b143897877b6916541ee1e
Patcherr <137843869+Patcherr@users.noreply.github.com>
2023-07-05 17:59:13 -0300

    Update register.js

--- Proyecto Final/JS/register.js
+++ Proyecto Final/JS/register.js
@@ -1,28 +1,29 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-  const nombre = document.getElementById('nombre').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+  const nombre = document.getElementById('nombre');
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
   const valid = document.getElementById('valid');
+  const returnLogin = document.getElementById('return');
 
   valid.addEventListener('click', function (event) {
     event.preventDefault();
 
-    if (nombre == '') {
+    if (nombre.value == '') {
       alert('Error en el nombre. Porfavor vuelva a ingresar');
-      
+
     }
-    if (email == '') {
+    /* if (email.value == '') {
       alert('Error en el email. Porfavor vuelva a ingresar');
-      
-    }
-    /* if (!/^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/.test(email)) {
+
+    } */
+    if (!/^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/.test(email)) {
       alert('Error en el email. Porfavor vuelva a ingresar');
       event.preventDefault();
-    } */
-    if (password == '') {
+    }
+    if (password.value == '') {
       alert('Error en el password. Porfavor vuelva a ingresar');
-      
+
     }
     /* // Mostrar un mensaje de éxito
     alert('Registro exitoso. ¡Ahora puedes iniciar sesión!'); */
@@ -30,6 +31,12 @@ window.addEventListener('DOMContentLoaded', function () {
     // Redireccionar a la página de inicio de sesión después del registro
     /*   */
   })
+  returnLogin.addEventListener('click', function () {
+
+    window.location.href = 'index.html';
+  })
+
+
 
 
 })
