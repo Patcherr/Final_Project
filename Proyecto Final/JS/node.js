32475f05798b993165e581e5e8539cbcd5fecca7
BlueLuscious <lucio.200152@gmail.com>
2023-07-10 19:50:05 -0300

    fixed patch bugs

--- Proyecto Final/JS/node.js
+++ Proyecto Final/JS/node.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', function () {
+<<<<<<< Updated upstream
     const userData = JSON.parse(localStorage.getItem('userData'))
     const name = document.getElementById('name');
     const email = document.getElementById('email');
@@ -13,3 +14,26 @@ window.addEventListener('DOMContentLoaded', function () {
       }
     })
   })
+=======
+
+  const name = document.getElementById('name');
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
+  const loginButton = document.getElementById('loginButton')
+  const loginForm = document.getElementById('loginForm');
+
+  const userData = JSON.parse(localStorage.getItem('User Data'));
+  if (userData) {
+    console.log('User Data:', userData);
+  }
+
+  loginButton.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    if (email.value == userData.email && password.value == userData.password) {
+      loginForm.submit();
+    }
+  })
+
+})
+>>>>>>> Stashed changes
